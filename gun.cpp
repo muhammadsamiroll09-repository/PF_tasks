7354c5e0275ebf32ab76ff2f8815e1d8f34f0b02
muhammadsamiroll09-repository <muhammadsamiroll.09@gmail.com>
2026-02-28 15:03:41 +0500

    week 2 tasks

---
+++ gun.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main(){
+system("color B");
+cout<<"+--^----------,--------,-----,--------^-,  "<<endl;
+cout<<" | |||||||||   ,________,     |          0 "<<endl;
+cout<<" '+---------------------------^----------| "<<endl;
+cout<<"   ',---------,---------,--------------'   "<<endl;
+cout<<"     / XXXXXX /'|      /'  "<<endl;
+cout<<"    / XXXXXX /  |     /'  "<<endl;
+cout<<"   / XXXXXX /'______'     "<<endl;
+cout<<"  / XXXXXX /         "<<endl;
+cout<<" / XXXXXX /         "<<endl;
+cout<<"(________(          "<<endl;
+cout<<" '______'           "<<endl;
+
+
+
+
+
+
+
+}
---
+++ pacmanblack.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"          .::_---::..  " << endl;
+cout<<"       .--------------.  " << endl;
+cout<<"     .------------------.  " << endl;
+cout<<"     -----------------:.  " << endl;
+cout<<"    :------------::.  " << endl;
+cout<<"    ------------:.  " << endl;
+cout<<"    :------------:..  " << endl;
+cout<<"      :---------------:.  " << endl;
+cout<<"       .-------------:  " << endl;
+cout<<"         .:--------:.  " << endl;
+}
